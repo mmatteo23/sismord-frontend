4ab606685edc85e0704f6c201506157745b6112d
Matteo <matteo.midena@protonmail.com>
2023-04-01 19:02:37 +0200

    Add error management on main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-// import './styles/globals.css';
 import {
   ZkConnectButton,
   ZkConnectClientConfig,
@@ -19,16 +17,16 @@ export const zkConnectConfig: ZkConnectClientConfig = {
   devMode: {
     // enable or disable dev mode here to create development groups and use the development vault.
     enabled: process.env.REACT_APP_ENV_NAME === "LOCAL" ? true : false,
-    // devGroups: [
-    //   {
-    //     groupId: "0x42c768bb8ae79e4c5c05d3b51a4ec74a",
-    //     // Add your dev addresses here to become eligible in the DEV env
-    //     data: [
-    //       "0x2bf7b04f143602692bbdc3ecbea68c2c65278eee",
-    //       "0x3f559454185098cb3a496f864a4bdd82b34c7fd1",
-    //     ],
-    //   },
-    // ],
+    devGroups: [
+      {
+        groupId: process.env.REACT_APP_GROUP_ID as string,
+        // Add your dev addresses here to become eligible in the DEV env
+        data: [
+          "0x3E612Ca41B0212c741ED7BdEE37B17a8CcefBcb4",
+          "0x5853bCAC824fc455C1e448706419633EFc452bC8",
+        ],
+      },
+    ],
   },
 };
 
@@ -37,90 +35,100 @@ enum SubscriptionStatus {
   NotSubscribed = "not-subscribed",
 }
 
-
-
 function App() {
   const [verifying, setVerifying] = useState(false);
   const [subscriptionStatus, setSubscriptionStatus] = useState<SubscriptionStatus | null>(null);
   const [zkConnectResponse, setZkConnectResponse] = useState<ZkConnectResponse | null>(null);
-  const [serverId, setServerId] = useState<string | null>(null);
-  const [discordId, setDiscordId] = useState<string | null>(null);
-  
-  console.log("APP id: ", process.env.REACT_APP_APP_ID)
-  console.log("Subscription status: ", subscriptionStatus)
-  
+  const [validPage, setValidPage] = useState(false);
+  const [botError, setBotError] = useState<string>("");
+  const [specificError, setSpecificError] = useState<string>("");
+
   const searchParams = new URLSearchParams(document.location.search)
   
   useEffect(() => {
-    if (searchParams.get("serverId")) {
+    if (searchParams.get("serverId") && searchParams.get("discordId")) {
       const serverId = searchParams.get("serverId")
       const discordId = searchParams.get("discordId")
-      // setServerId(serverId)
-      // setDiscordId(discordId)
-      // console.log(document.location.href)
-      // console.log(discordId as string + (document.location.href).split("#")[1])
       localStorage.setItem("serverId", serverId as string)
       localStorage.setItem("discordId", discordId as string + "#" + (document.location.href).split("#")[1])
     } 
+
+    if (localStorage.getItem("serverId") && localStorage.getItem("discordId")) {
+      setValidPage(true)
+    }
   }, [searchParams])
 
-
-
-
-  console.log("Server id: ", serverId)
-  console.log("Discord id: ", discordId)
-
-  return (
-    <Main>
-      {!subscriptionStatus && (
-        <>
-          {/* put the img discordchads.svg */}
-          <DiscordChads style={{width: "30%", height: "30%"}}/>
-          <Title id="title">Verify your profile to gain access to the exclusive server ⚡</Title>
-          <ZkConnectButton
-            config={zkConnectConfig}
-            authRequest={{authType: AuthType.ANON}}
-            // claimRequest={{
-            //   //The merge contributor groupId
-            //   groupId: process.env.NEXT_PUBLIC_SISMO_GROUP_ID,
-            // }}
-            onResponse={(response) => {
-              setVerifying(true);
-              setZkConnectResponse(response);
-              axios
-                .post(`http://localhost:3333/`+ localStorage.getItem("serverId") +`/verify`, {
-                  discordId: localStorage.getItem("discordId"),
-                  zkConnectResponse: response,
-                })
-                .then((res) => {
-                  console.log(res)
-                  setVerifying(false);
-                  setSubscriptionStatus(SubscriptionStatus.AlreadySubscribed);
-                })
-                .catch((err) => {
-                  setVerifying(false);
-                });
-            }}
-            verifying={verifying}
-            overrideStyle={{
-              marginTop: 30,
-              background: "#715aff",
-              border: "1px solid #8b8b8b"
-            }}
-          />
-        </>
-      )}
-      {subscriptionStatus && (
-        <>
-          <DiscordChads style={{width: "30%", height: "30%"}}/>
-          <Title>You are officially a member of the Discord server!</Title>
-          <Subtitle id="success-subtitle">
-            If you come back to it, you can see you gained the special role <b>Chad</b> 🗿🍷
-          </Subtitle>
-        </>
-      )}
-    </Main>
-  );
+  if(validPage) {
+    return (
+      <Main>
+        {botError && (
+          <>
+            <Title>{specificError}</Title>
+            <Subtitle>{botError}</Subtitle>
+            <Subtitle>Whoooopsss! Seems like something went wrong with the bot!</Subtitle>
+          </>
+        )}
+        {(!subscriptionStatus && !botError) && (
+          <>
+            {/* put the img discordchads.svg */}
+            <DiscordChads style={{width: "30%", height: "30%"}}/>
+            <Title id="title">Verify your profile to gain access to the exclusive server ⚡</Title>
+            <ZkConnectButton
+              config={zkConnectConfig}
+              authRequest={{authType: AuthType.ANON}}
+              claimRequest={{
+                //The merge contributor groupId
+                groupId: process.env.REACT_APP_GROUP_ID as string,
+              }}
+              onResponse={(response) => {
+                setVerifying(true);
+                setZkConnectResponse(response);
+                axios
+                  .post(`http://localhost:3333/`+ localStorage.getItem("serverId") +`/verify`, {
+                    discordId: localStorage.getItem("discordId"),
+                    zkConnectResponse: response,
+                  })
+                  .then((res) => {
+                    console.log(res)
+                    setVerifying(false);
+                    setSubscriptionStatus(SubscriptionStatus.AlreadySubscribed);
+                  })
+                  .catch((err) => {
+                    console.log(err)
+                    setVerifying(false);
+                    setBotError(err.message)
+                    setSpecificError(err.response.data)
+                  });
+              }}
+              verifying={verifying}
+              overrideStyle={{
+                marginTop: 30,
+                background: "#715aff",
+                border: "1px solid #8b8b8b"
+              }}
+            />
+          </>
+        )}
+        {(subscriptionStatus && !botError) && (
+          <>
+            <DiscordChads style={{width: "30%", height: "30%"}}/>
+            <Title>You are officially a member of the Discord server!</Title>
+            <Subtitle id="success-subtitle">
+              If you come back to it, you can see you gained the special role <b>Chad</b> 🗿🍷
+            </Subtitle>
+          </>
+        )}
+      </Main>
+    );
+  } else {
+    return (
+      <Main>
+        <Title>Invalid page</Title>
+        <Subtitle>Whoooopsss! Seems like Discord server and User identifier are missing!</Subtitle>
+        <p>You can't be a Discord chad without your links</p>
+      </Main>
+    )
+  }
 }
 
 export default App;
